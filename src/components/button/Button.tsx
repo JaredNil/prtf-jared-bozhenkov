edb452e9dc809ce52c307c8d3686d4ef5a4a9e57
JN <jared.bozh@gmail.com>
2023-03-25 00:58:36 +0800

    anime redit

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,27 +1,32 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { buttonData } from '../../types';
 
 import classes from '../header/header.module.scss'
 
 
 const Button: React.FC<buttonData> = (props) => {
-	const buttonRef = useRef<HTMLDivElement | any>('s')
+	const buttonRef = useRef<HTMLDivElement | any>()
+	let buttonRefClasses: any
+	useEffect(() => {
+		buttonRefClasses = buttonRef.current.children[0].classList
+	}, [buttonRef])
 
-	const mouseUpHandler = () => {
-		buttonRef.current.children[0].classList.add(classes.button__hover_mouseMove)
+
+	const mouseMoveHandler = () => {
+		buttonRefClasses.add(classes.button__hover_mouseMove)
 	}
 
-	const mouseDownHandler = () => {
-		buttonRef.current.children[0].classList.remove(classes.button__hover_mouseMove)
-		buttonRef.current.children[0].classList.toggle(classes.button__hover_mouseStart)
-		buttonRef.current.children[0].classList.toggle(classes.button__hover_mouseEnd)
+	const mouseLeaveHandler = () => {
+		buttonRefClasses.remove(classes.button__hover_mouseMove)
+		buttonRefClasses.toggle(classes.button__hover_mouseStart)
+		buttonRefClasses.toggle(classes.button__hover_mouseEnd)
 	}
 
 	return (
 		<div className={classes.button} >
 			<div ref={buttonRef}
 				className={`${classes.button__container} ${classes[props.theme]}`}
-				onMouseMove={mouseUpHandler} onMouseLeave={mouseDownHandler}
+				onMouseMove={mouseMoveHandler} onMouseLeave={mouseLeaveHandler}
 			>
 				<span className={classes.button__hover + ' ' + classes.button__hover_mouseStart}></span>
 				<div className={classes.button__icon}>ic</div>
